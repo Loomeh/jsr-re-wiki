e0b9edeb879902397f9e33cd817dbcf156818439
Zane Holbrooke-Jones <67561520+Loomeh@users.noreply.github.com>
2025-04-17 22:25:43 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/jsr-re-wiki/docs/intro" />;
+  return <Redirect to="/docs/intro" />;
 };
